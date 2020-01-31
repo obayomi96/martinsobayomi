c3d48e608df62ef31571ae9ab570abc045946073
martins obayomi <obayomimartins96@gmail.com>
2020-01-31 08:05:30 +0100

    Navbar $ biography done

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -10,12 +10,12 @@ const LandingPage = () => {
 		<div className="flex">
 				<div className="column">
 					<p className="about">
-						I'm a Fullstack Software Engineer and a technology enthusiast who is passionate about problem solving and adapts well to change.<br/>
-						I'm a diverse individual who loves to integrate and contribute meaningfully to the progress of any team I'm opportune to work with.<br/>
-						My objectives are to create meaningful and useful products, and be a valuable member among my stakeholders.<br/>
+						I'm a Fullstack Software Engineer and a technology enthusiast who is passionate about problem solving and adapts well to change.
+						I'm a diverse individual who loves to integrate and contribute meaningfully to the progress of any team I'm opportune to work with.
+						My objectives are to create meaningful and useful products, and be a valuable member among my stakeholders.
 					</p>
 					<div className="image">
-						<img src={imgSrc} style={{border: '3px grey solid', borderRadius: '4px'}} className="img" alt="Martins Obayomi profile photo" height="250px" width="60px" />
+						<img src={imgSrc} style={{border: '3px grey solid', borderRadius: '50%'}} className="img" alt="Martins Obayomi profile photo" height="500px" width="500px" />
 					</div>
         </div>
     </div>
